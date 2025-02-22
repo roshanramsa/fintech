bacd395e3caf5fd377bfab638d30aace6556f6cc
roshanramsa <roshanramsa@gmail.com>
2025-02-22 13:13:28 +0530

    Delete JavaScript/Start directory

--- JavaScript/Start/main.js
+++
@@ -1,8 +0,0 @@
-var n = 100;
-for (var i = 0; i<2*n+1; i++){
-    if (i != 0 && i!=2*n)
-        console.log(" ".repeat(Math.abs(n-i)) + "*" + " ".repeat( 2*(n-Math.abs(n-i))-1) + "*");
-    else
-        console.log(" ".repeat(Math.abs(n-i)) + "*")
-}
-
--- JavaScript/Start/trial.js
+++
@@ -1,4 +0,0 @@
-var arr = [1,"Hello"];
-for (var i = 0; i<arr.length; i++){
-    console.log(arr[i]);
-}
